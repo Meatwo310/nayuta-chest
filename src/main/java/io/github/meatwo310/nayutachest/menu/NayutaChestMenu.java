8cb28b2265a8df3ea1cac9aeea0a361c041d7c34
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-01-18 19:28:26 +0900

    クイック移動を実装

--- src/main/java/io/github/meatwo310/nayutachest/menu/NayutaChestMenu.java
+++ src/main/java/io/github/meatwo310/nayutachest/menu/NayutaChestMenu.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import io.github.meatwo310.nayutachest.block.ModBlocks;
 import io.github.meatwo310.nayutachest.blockentity.NayutaChestBE;
 import io.github.meatwo310.nayutachest.handler.NayutaChestDisplayHandler;
+import io.github.meatwo310.nayutachest.handler.NayutaChestHandler;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -14,6 +15,8 @@ import net.minecraftforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
+import java.util.Arrays;
+
 public class NayutaChestMenu extends AbstractContainerMenu {
     private static final Logger LOGGER = LogUtils.getLogger();
 
@@ -21,6 +24,10 @@ public class NayutaChestMenu extends AbstractContainerMenu {
     private final ContainerLevelAccess containerLevelAccess;
     public final ContainerData containerData;
     //    private final ContainerData containerData;
+
+    private final int INPUT_SLOT;
+    private final int OUTPUT_SLOT;
+    private final int DISPLAY_SLOT;
 
     // Client
     public NayutaChestMenu(int containerId, Inventory playerInventory, FriendlyByteBuf extraData) {
@@ -46,7 +53,10 @@ public class NayutaChestMenu extends AbstractContainerMenu {
 
         createPlayerHotbar(playerInventory);
         createPlayerInventory(playerInventory);
-        createBESlots(be);
+        int[] beSlots = createBESlots(be);
+        INPUT_SLOT = beSlots[0];
+        OUTPUT_SLOT = beSlots[1];
+        DISPLAY_SLOT = beSlots[2];
 
         addDataSlots(containerData);
     }
@@ -70,11 +80,32 @@ public class NayutaChestMenu extends AbstractContainerMenu {
         }
     }
 
-    private void createBESlots(NayutaChestBE blockEntity) {
+    private int[] createBESlots(NayutaChestBE blockEntity) {
+        int[] slotIndexes = new int[3];
+        Arrays.fill(slotIndexes, -1);
+
         blockEntity.displayHandlerLazyOptional.ifPresent(displayHandler -> {
-            this.addSlot(new SlotItemHandler(displayHandler, NayutaChestDisplayHandler.SLOT_INPUT, 35, 35));
-            this.addSlot(new SlotItemHandler(displayHandler, NayutaChestDisplayHandler.SLOT_OUTPUT, 125, 35));
-            this.addSlot(new SlotItemHandler(displayHandler, NayutaChestDisplayHandler.SLOT_DISPLAY, 80, 35));
+            Slot input = this.addSlot(new SlotItemHandler(
+                    displayHandler,
+                    NayutaChestDisplayHandler.SLOT_INPUT,
+                    35,
+                    35
+            ));
+            Slot output = this.addSlot(new SlotItemHandler(
+                    displayHandler,
+                    NayutaChestDisplayHandler.SLOT_OUTPUT,
+                    125,
+                    35
+            ));
+            Slot display = this.addSlot(new SlotItemHandler(
+                    displayHandler,
+                    NayutaChestDisplayHandler.SLOT_DISPLAY,
+                    80,
+                    35
+            ));
+            slotIndexes[0] = input.index;
+            slotIndexes[1] = output.index;
+            slotIndexes[2] = display.index;
 
             StringBuilder stackLog = new StringBuilder();
             for (int i = 0; i < displayHandler.getSlots(); i++) {
@@ -90,16 +121,141 @@ public class NayutaChestMenu extends AbstractContainerMenu {
                 LOGGER.debug(beStackLog.toString());
             });
         });
+
+        return slotIndexes;
     }
 
     @Override
     @NotNull
-    public ItemStack quickMoveStack(@NotNull Player player, int i) {
-        return ItemStack.EMPTY;
+    public ItemStack quickMoveStack(@NotNull Player player, int slotIndex) {
+        Slot slot = this.getSlot(slotIndex);
+        ItemStack itemStack = slot.getItem();
+
+        if (itemStack.isEmpty()) {
+            return ItemStack.EMPTY;
+        }
+
+        ItemStack itemStackCopy = itemStack.copy();
+
+        boolean success;
+        if (slotIndex < Inventory.INVENTORY_SIZE) {
+            success = this.movePlayerToContainer(player, slotIndex, itemStack);
+        } else {
+            success = this.moveContainerToPlayer(player, slotIndex, itemStack);
+//            return ItemStack.EMPTY;
+        }
+        if (!success) {
+            return ItemStack.EMPTY;
+        }
+
+        slot.setChanged();
+        slot.onTake(player, itemStack);
+
+        return itemStackCopy;
+    }
+
+    public boolean movePlayerToContainer(Player player, int slotIndex, ItemStack itemStack) {
+        // Move inventory item (#0-35) to BE slot (#36)
+        return this.moveBetweenInventoryAndBE(
+                itemStack,
+                36,
+                37,
+                false
+        );
+    }
+
+    public boolean moveContainerToPlayer(Player player, int slotIndex, ItemStack itemStack) {
+        // Prevent moving display slot (#38) to inventory
+        if (slotIndex >= DISPLAY_SLOT) return false;
+
+        // Move BE item (#36-38) to inventory slot (#0-35)
+        return this.moveBetweenInventoryAndBE(
+                itemStack,
+                0,
+                Inventory.INVENTORY_SIZE,
+                false
+        );
     }
 
     @Override
     public boolean stillValid(@NotNull Player player) {
         return stillValid(this.containerLevelAccess, player, ModBlocks.NAYUTA_CHEST.get());
+    }
+
+    private boolean moveBetweenInventoryAndBE(@NotNull ItemStack stack, int start, int size, boolean lastToFirst) {
+        boolean moved = false;
+        for (int i = (lastToFirst ? size - 1 : start); (lastToFirst ? i >= start : i < size); i += (lastToFirst ? -1 : 1)) {
+            if (stack.isEmpty()) {
+                break;
+            }
+            if (!this.canInsert(i, stack)) {
+                continue;
+            }
+            NayutaChestHandler chestHandler = this.nayutaChestBlock.chestHandlerLazyOptional.orElseThrow(() ->
+                    new IllegalStateException("chestHandlerLazyOptional is not present")
+            );
+            ItemStack stackInSlot = chestHandler.getStackInSlot(NayutaChestHandler.SLOT_OUTPUT);
+            if (!stackInSlot.isEmpty() && !ItemStack.isSameItemSameTags(stack, stackInSlot)) {
+                continue;
+            }
+            Slot targetSlot = this.slots.get(i);
+            ItemStack targetStack = targetSlot.getItem();
+            if (i < Inventory.INVENTORY_SIZE) {
+                int toExtract = Math.min(stack.getCount(), targetSlot.getMaxStackSize() - targetStack.getCount());
+                ItemStack extracted = chestHandler.extractItem(NayutaChestHandler.SLOT_OUTPUT, toExtract, false);
+                if (extracted.isEmpty()) {
+                    continue;
+                }
+                stack.shrink(extracted.getCount());
+                if (targetStack.isEmpty()) {
+                    targetSlot.setByPlayer(extracted);
+                } else {
+                    targetStack.grow(extracted.getCount());
+                }
+            } else {
+                ItemStack remaining = chestHandler.insertItem(NayutaChestHandler.SLOT_INPUT, stack.copy(), false);
+                if (remaining.getCount() >= stack.getCount()) {
+                    continue;
+                }
+                stack.shrink(stack.getCount() - remaining.getCount());
+            }
+            targetSlot.setChanged();
+            moved = true;
+        }
+        return moved;
+    }
+
+    private boolean canInsert(int slotIndex, ItemStack stack) {
+        if (slotIndex < 0) {
+            return false;
+        } else if (slotIndex < Inventory.INVENTORY_SIZE) {
+            return canInventoryInsert(slotIndex, stack);
+        } else if (slotIndex == INPUT_SLOT) {
+            return canBEInsert(slotIndex, stack);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean canBEInsert(int slotIndex, ItemStack stack) {
+        if (slotIndex != INPUT_SLOT || !this.nayutaChestBlock.chestHandlerLazyOptional.isPresent()) {
+            return false;
+        }
+        NayutaChestHandler chestHandler = this.nayutaChestBlock.chestHandlerLazyOptional.orElseThrow(() ->
+                new IllegalStateException("chestHandlerLazyOptional is present but not present")
+        );
+        ItemStack remaining = chestHandler.insertItem(NayutaChestHandler.SLOT_INPUT, stack, true);
+        return remaining.getCount() < stack.getCount();
+    }
+    
+    private boolean canInventoryInsert(int slotIndex, ItemStack stack) {
+        if (slotIndex < 0 || slotIndex >= Inventory.INVENTORY_SIZE) {
+            return false;
+        }
+        Slot targetSlot = this.slots.get(slotIndex);
+        ItemStack targetStack = targetSlot.getItem();
+        return (targetStack.isEmpty() && targetSlot.mayPlace(stack) ||
+                ItemStack.isSameItemSameTags(stack, targetStack) && targetStack.getCount() < targetSlot.getMaxStackSize()
+        );
     }
 }
